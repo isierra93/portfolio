22c1daba8242503774fcad25548e3718f8502979
Ivan Sierra <sierraivan93@gmail.com>
2023-11-12 17:43:35 -0300

    sendEmail

--- src/services/sendEmail.js
+++ src/services/sendEmail.js
@@ -1,9 +1,9 @@
 import emailjs from "@emailjs/browser"
 import Swal from "sweetalert2"
 
-const EMAIL_SERVICE_KEY = import.meta.env.VITE_EMAIL_SERVICE_KEY
-const EMAIL_TEMPLATE_KEY = import.meta.env.VITE_EMAIL_TEMPLATE_KEY
-const PUBLIC_KEY = import.meta.env.VITE_PUBLIC_KEY
+const EMAIL_SERVICE_KEY = 'service_6n1g5ug'
+const EMAIL_TEMPLATE_KEY = 'template_pq7t16l'
+const PUBLIC_KEY = 'M3Bj2wy194z-IY7dS'
 
 export const sendEmail = async (e) => {
     await emailjs.sendForm(EMAIL_SERVICE_KEY, EMAIL_TEMPLATE_KEY, e.target, PUBLIC_KEY)
